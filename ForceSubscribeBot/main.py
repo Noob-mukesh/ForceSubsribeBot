1547dea361f616bac185054dbc68af9a19055094
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-22 22:31:40 +0530

    Update main.py

--- ForceSubscribeBot/main.py
+++ ForceSubscribeBot/main.py
@@ -44,7 +44,7 @@ async def main(bot: Client, msg: Message):
                 if action == 'mute':
                     await msg.chat.restrict_member(user_id, ChatPermissions(can_send_messages=False))
                     buttons.append([InlineKeyboardButton("Unmute Me", callback_data=f"joined+{msg.from_user.id}")])
-                await msg.reply(
+                await message.from_user.username(
                     f"You must join {mention} to chat here.",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup(buttons),
