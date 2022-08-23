519cbdb6dd6f70ea1adbf5406cff9a3e1694ed1e
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-23 06:21:39 +0530

    Update id.py

--- ForceSubscribeBot/id.py
+++ ForceSubscribeBot/id.py
@@ -3,4 +3,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.text & filters.incoming & filters.command("id"))
 async def id(_, msg):
-    await msg.reply(f"Chat ID is : `{msg.chat.id}`", quote=True)
+    await msg.reply(f"ᴄʜᴀᴛ ɪᴅ ɪs : `{msg.chat.id}`", quote=True)
