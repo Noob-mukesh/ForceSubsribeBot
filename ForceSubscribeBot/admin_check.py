629e349e83fbc12b1f3146ba60338cef07edd083
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-23 10:47:49 +0530

    Update admin_check.py

--- ForceSubscribeBot/admin_check.py
+++ ForceSubscribeBot/admin_check.py
@@ -3,12 +3,12 @@ async def admin_check(bot, msg, user_id=None, callback_query=None):
         user_id = msg.from_user.id
     bot_id = (await bot.get_me()).id
     if msg.chat.type not in ["supergroup", "group"]:
-        await msg.reply("This command can only be used in Groups !", quote=True)
+        await msg.reply(" ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ɪɴ ɢʀᴏᴜᴘs  ʙᴀʙʏ ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ!", quote=True)
         return False
     chat_member = await msg.chat.get_member(user_id)
     bot_chat_member = await msg.chat.get_member(bot_id)
     if bot_chat_member.status != "administrator":
-        text = "Please make me admin and then try again !"
+        text = "ᴘʟᴇᴀsᴇ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ᴡɪᴛʜ ʙᴀɴ ᴘᴏᴡᴇʀ ᴛʜᴇɴ ᴛʀʏ ᴀɢᴀɪɴ!"
         if callback_query:
             await callback_query.answer(text, show_alert=True)
         else:
