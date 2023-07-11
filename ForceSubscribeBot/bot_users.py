7461fad170495b03638b47da73157d95390dbf88
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-11 23:01:10 +0530

    Update bot_users.py

--- ForceSubscribeBot/bot_users.py
+++ ForceSubscribeBot/bot_users.py
@@ -3,9 +3,9 @@ from ForceSubscribeBot.database.chats_sql import num_chats
 from ForceSubscribeBot.database import SESSION
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from config import OWNER_ID
 
-
-@Client.on_message(~filters.edited & ~filters.service, group=1)
+@Client.on_message( ~filters.service, group=1)
 async def users_sql(_, msg: Message):
     if msg.from_user:
         q = SESSION.query(Users).get(int(msg.from_user.id))
@@ -16,7 +16,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(2145093972) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     chats = await num_chats()
