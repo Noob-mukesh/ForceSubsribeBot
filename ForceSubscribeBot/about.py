44bc3ba2c97f66cd91b652d54b66249adb2ba0c0
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-11 23:06:54 +0530

    Update about.py

--- ForceSubscribeBot/about.py
+++ ForceSubscribeBot/about.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup
 
 
 # About Message
-@Client.on_message(filters.private & filters.incoming & filters.command("about"))
+@Client.on_message(filters.incoming & filters.command("about"))
 async def about(bot, msg):
     await bot.send_message(
         msg.chat.id,
