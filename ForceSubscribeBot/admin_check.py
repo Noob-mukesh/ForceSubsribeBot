94913fd0d3ea3487ae109b9128d8e611e09610c6
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-13 17:09:25 +0530

    Update admin_check.py

--- ForceSubscribeBot/admin_check.py
+++ ForceSubscribeBot/admin_check.py
@@ -1,23 +1,22 @@
 
-from pyrogram import types
-
+from pyrogram import enums
 async def admin_check(client, message, user_id=None, callback_query=None):
     if not user_id:
         user_id = message.from_user.id
     bot_id = (await client.get_me()).id
-    if message.chat.type not in [types.ChatType.SUPERGROUP, types.ChatType.GROUP]:
+    if message.chat.type not in [enums.ChatType.SUPERGROUP, enums.ChatType.GROUP]:
         await message.reply_text("This command can only be used in groups.", quote=True)
         return False
     chat_member = await client.get_chat_member(message.chat.id, user_id)
     bot_chat_member = await client.get_chat_member(message.chat.id, bot_id)
-    if bot_chat_member.status != types.ChatMemberStatus.ADMINISTRATOR:
+    if bot_chat_member.status != enums.ChatMemberStatus.ADMINISTRATOR:
         text = "Please make me an admin with ban power and try again!"
         if callback_query:
             await callback_query.answer(text, show_alert=True)
         else:
             await message.reply_text(text)
         return False
-    if chat_member.status not in (types.ChatMemberStatus.CREATOR, types.ChatMemberStatus.ADMINISTRATOR):
+    if chat_member.status not in (enums.ChatMemberStatus.CREATOR, enums.ChatMemberStatus.ADMINISTRATOR):
         text = "This command is only for admins!"
         if callback_query:
             await callback_query.answer(text, show_alert=True)
