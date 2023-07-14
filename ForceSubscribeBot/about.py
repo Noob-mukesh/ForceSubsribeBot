ed7065e6b13806ac73905a99448c23fd14eb62b3
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-14 18:37:17 +0530

    Update about.py

--- ForceSubscribeBot/about.py
+++ ForceSubscribeBot/about.py
@@ -1,11 +1,12 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
-from Config import BOT_USERNAME
+from Config import BOT_USERNAME,START_IMG,
+
 
 @Client.on_message(filters.command(["about",f"@{BOT_USERNAME}about"]))
 async def about(bot, msg):
-    await msg.reply(
+    await msg.reply_phto(START_IMG,
         Data.ABOUT,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
