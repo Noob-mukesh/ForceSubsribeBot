55df2a2d2697bbcd34cde4c07cbc0a059731801a
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-14 20:25:22 +0530

    Update about.py

--- ForceSubscribeBot/about.py
+++ ForceSubscribeBot/about.py
@@ -6,7 +6,7 @@ from Config import BOT_USERNAME,START_IMG
 
 @Client.on_message(filters.command(["about",f"@{BOT_USERNAME}about"]))
 async def about(bot, msg):
-    await msg.reply_phto(START_IMG,
+    await msg.reply_photo(START_IMG,
         Data.ABOUT,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
