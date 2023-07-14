cb40eee51d1c4d6c80402edadc04737191f8adc8
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-14 20:21:59 +0530

    Update about.py

--- ForceSubscribeBot/about.py
+++ ForceSubscribeBot/about.py
@@ -1,7 +1,7 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
-from Config import BOT_USERNAME,START_IMG,
+from Config import BOT_USERNAME,START_IMG
 
 
 @Client.on_message(filters.command(["about",f"@{BOT_USERNAME}about"]))
