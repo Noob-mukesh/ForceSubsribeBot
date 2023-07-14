238d63e0a60937bd267d3bfe671f081973d3f9ce
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-14 18:36:31 +0530

    Update help.py

--- ForceSubscribeBot/help.py
+++ ForceSubscribeBot/help.py
@@ -1,13 +1,13 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
-
+from Config import START_IMG
 
 # Help Message
 @Client.on_message(filters.command("help"))
 async def _help(bot, msg):
-    await bot.send_message(
-        msg.chat.id,
+    await msg.reply_photo(
+        START_IMG,
         "**ʜᴇʀᴇ's ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ ?**\n" + Data.HELP,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
