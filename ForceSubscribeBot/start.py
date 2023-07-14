8c4b0204387b97e7ae5b7cc91e2df3ca6541e183
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-14 18:34:12 +0530

    Update start.py

--- ForceSubscribeBot/start.py
+++ ForceSubscribeBot/start.py
@@ -1,14 +1,14 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
-
+from Config import START_IMG
 
 # Start Message
 @Client.on_message(filters.command("start"))
 async def start(bot, msg):
 	user = await bot.get_me()
 	mention = user.mention
-	await msg.reply_phpto(
+	await msg.reply_phpto(START_IMG,
 		Data.START.format(msg.from_user.mention, mention),
 		reply_markup=InlineKeyboardMarkup(Data.buttons)
 	)
