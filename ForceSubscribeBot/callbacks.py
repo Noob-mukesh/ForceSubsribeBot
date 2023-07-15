fa1979ab30618d8118cf2196240acac6517f64e9
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-15 23:48:19 +0530

    Update callbacks.py

--- ForceSubscribeBot/callbacks.py
+++ ForceSubscribeBot/callbacks.py
@@ -20,12 +20,12 @@ from ForceSubscribeBot.settings import action_markup
 @Client.on_callback_query()
 async def _callbacks(bot: Client, callback_query: CallbackQuery):
     user = await bot.get_me()
-    user_id = callback_query.message.chat.id
+    user_id = callback_query.from_user.id
     mention = user.mention
     query = callback_query.data.lower()
     if query.startswith("home"):
         if query == 'home':
-            chat_id = callback_query.message.chat.id
+            chat_id = callback_query.from_user.id
             message_id = callback_query.message.id
             await bot.edit_message_text(
                 chat_id=chat_id,
@@ -34,7 +34,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
                 reply_markup=InlineKeyboardMarkup(Data.buttons),
             )
     elif query == "about":
-        chat_id = callback_query.message.chat.id
+        chat_id = callback_query.from_user.id
         message_id = callback_query.message.id
         await bot.edit_message_text(
             chat_id=chat_id,
@@ -44,7 +44,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
     elif query == "help":
-        chat_id = callback_query.message.chat.id
+        chat_id = callback_query.from_user.id
         message_id = callback_query.message.id
         await bot.edit_message_text(
             chat_id=chat_id,
